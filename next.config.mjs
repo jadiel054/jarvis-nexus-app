5076a945442bcedb32b0c726fb8cafd68c973f61
jadiel054 <jadielalves54@gmail.com>
2026-06-11 18:56:55 -0300

    fix: next.config.mjs sem TypeScript

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   experimental: { serverActions: { allowedOrigins: ["*"] } },
   images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
 };
